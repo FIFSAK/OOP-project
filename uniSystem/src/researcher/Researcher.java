569ba16fd3dc7779149df98fa04e96fe44bb2df8
FIFSAK <anuar200572@gmail.com>
2023-12-13 22:51:39 +0600

    add overload newProject for ResearcherDecorator, UsernameComparator, update with new funtions  and compare with uml, updarte uml by last version of researcher package

--- uniSystem/src/researcher/Researcher.java
+++ uniSystem/src/researcher/Researcher.java
@@ -1,8 +1,15 @@
 package researcher;
 
+import java.util.Vector;
+
 public interface Researcher {
 	public void addProject(ResearchProject researchProject);
 	public String printPapers(String sortType);
 	public void calculateHIndex() throws LowHIndex;
-	
+	public void newProject(String topic, Vector<ResearchPaper> publishedPapers, Vector<ResearcherDecorator> participants);
+	public void newPaper(ResearchPaper rp);
+	public void newProject(ResearchProject rp);
+	public void newPaper(String name, 
+			Vector<ResearchPaper> citations,
+			int pages, String journal);
 }
